d2b45b66a3e850528e35931d41395db391778ec8
Jacob Burns <jacobburns@live.co.uk>
2016-09-13 13:59:57 +0100

    Added board

--- tack.js
+++ tack.js
@@ -1,3 +1,25 @@
 window.onload = function(){
+	var buttons = Array.from(document.getElementsByTagName("td"));
+	var turnCounter = 1;
+	var board = [["e", "e", "e"], ["e", "e", "e"], ["e", "e", "e"]];
 
-}
+	buttons.forEach(function(button) {
+		button.addEventListener("click", function(event) {
+			
+			console.log(this.id);
+			var row = this.id.split(".")[0];
+	    	var column = this.id.split(".")[1];
+
+			console.log("Row = " + row + " Column = " + column);
+
+			if ((turnCounter % 2) > 0) {
+				event.target.innerText = "X";
+			}
+			else {
+				event.target.innerText = "O";
+			}
+			turnCounter++;
+
+		});
+	});
+};
