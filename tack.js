6fc0501c627e09d962e5e8b6f357da2e62bf0474
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2016-09-14 09:29:18 +0100

    comments

--- tack.js
+++ tack.js
@@ -39,14 +39,21 @@ window.onload = function(){
 	buttons.forEach(function(button) {
 		button.addEventListener("click", function(event) {
 			
+			//if game is active
 			if (inPlay) {
+				//allow player to play
 				updateBoard(getRow(this.id), getColumn(this.id));
+				//if the player has made a winning move
 				if (checkWin(this.id)) {
+					//deactivate game
 					inPlay = false;
+					//display win message
 					displayWinner("CONGRATZ PLAYER " + curPlayer);
 					console.log(curPlayer);
+					//update the score board
 					updateScore();
 				}
+				//else player has not won so switch player
 				else{
 					changePlayer();
 					console.log(curPlayer);
@@ -58,32 +65,42 @@ window.onload = function(){
 
 
 	function updateBoard(row, column) {
+		//if board position is empty
 		if ((board[row][column])==="e") {
+			//use turn counter to work out which player 
 			if ((turnCounter % 2) > 0) {
+				//if odd it is player x
+				//display on board
 				event.target.innerText = "X";
+				//update array
 				board[row][column] = "X";
 			}
 			else {
+				//even is player o
+				//display on board
 				event.target.innerText = "O";
+				//update array
 				board[row][column] = "O";
 			}
+			//increase turn counter
 			turnCounter++;
 		}
-		console.log("Row = " + row + " Column = " + column);
-
 	};
 
 	function getRow(position) {
+		//get the row from the <td> id
 		var row = position.split(".")[0];
 		return row;
 	};
 
 	function getColumn(position) {
+		//get the column from the <td> id
 		var column = position.split(".")[1];
 		return column;
 	};
 
 	function checkWin(position) {
+		//check if player has won
 		if (checkRow(getRow(position))) {
 			return true;
 		}
@@ -93,6 +110,7 @@ window.onload = function(){
 		else if (checkDiagonal()) {
 			return true;
 		}
+		//none of checks trigger so player has not won
 		else {
 			return false;
 		}
@@ -101,6 +119,7 @@ window.onload = function(){
 	}
 
 	function checkRow(row) {
+		//compare values in row
 		if ((board[row][0] === board[row][1]) && (board[row][0] ===board[row][2])) {
 			return true;
 		}
@@ -110,6 +129,7 @@ window.onload = function(){
 	};
 
 	function checkColumn(column) {
+		//compare values in column
 		if ((board[0][column] === board[1][column]) && (board[0][column] === board[2][column])) {
 			return true;
 		}
@@ -119,6 +139,7 @@ window.onload = function(){
 	};
 
 	function checkDiagonal() {
+		//check diagonals for a winner
 		if (board[1][1] !== "e") {
 			if (((board[0][0] === board[1][1]) && (board[0][0] === board[2][2])) || ((board[2][0] === board[1][1]) && (board[2][0] === board[0][2]))) {
 				return true;
@@ -130,14 +151,17 @@ window.onload = function(){
 	}
 
 	function displayWinner(message) {
+		//display win message
+		//insert message into page
 		document.getElementById("winner").innerText = message;
+		//add class to board <td> elements
 		buttons.forEach(function(button) {
 			button.className = "boardWin";
 		});
 	}
 
 	function changePlayer() {
-		// use ternary
+		//TODO use ternary
 		if (curPlayer == player1) {
 			curPlayer = player2;
 		} 
@@ -150,12 +174,16 @@ window.onload = function(){
 
 	function updateScore() {
 		if (curPlayer == player1) {
+			//update score
 			player1Score++;
+			//update page
 			document.getElementById("player1Score").innerText = player1Score;
 		} 
 		else {
 			if (curPlayer == player2) {
+				//update score
 				player2Score++;
+				//update page
 				document.getElementById("player2Score").innerText = player2Score;
 			}
 		}
